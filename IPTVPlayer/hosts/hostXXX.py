7a1854743ee70cdf143e763caf6e005f29ec0461
mamrot <wojtek2000@konto.pl>
2013-08-03 22:56:57 +0200

    eporner

--- IPTVPlayer/hosts/hostXXX.py
+++ IPTVPlayer/hosts/hostXXX.py
@@ -116,7 +116,7 @@ class IPTVHost(IHost):
     ###################################################
 
 class Host:
-    XXXversion = "6.0.0.0"
+    XXXversion = "7.0.0.0"
     XXXremote  = "0.0.0.0"
     currList = []
     MAIN_URL = ''
@@ -176,6 +176,69 @@ class Host:
            valTab.append(CDisplayListItem('PORNHUB',        'www.pornhub.com',    CDisplayListItem.TYPE_CATEGORY, ['http://www.pornhub.com/categories'],    'fullpornhub', 'http://cdn1.static.pornhub.phncdn.com/images/pornhub_logo.png', None)) 
            valTab.append(CDisplayListItem('HDPORN',         'www.hdporn.net',     CDisplayListItem.TYPE_CATEGORY, ['http://www.hdporn.net/channels/'],      'hdporn',  'http://www.hdporn.net/gfx/logo.gif', None)) 
            valTab.append(CDisplayListItem('4TUBE',          'www.4tube.com',      CDisplayListItem.TYPE_CATEGORY, ['http://www.4tube.com/videos'],          '4tube',   'http://i.4tube.com/free-porn.png', None)) 
+           valTab.append(CDisplayListItem('EPORNER',        'www.eporner.com',    CDisplayListItem.TYPE_CATEGORY, ['http://www.eporner.com/categories/'],   'eporner', 'http://static.eporner.com/new/logo.png', None)) 
+           printDBG( 'Host listsItems end' )
+           return valTab
+
+        # ########## #
+        if 'eporner' == name:
+           printDBG( 'Host listsItems begin name='+name )
+           self.MAIN_URL = 'http://www.eporner.com' 
+           query_data = { 'url': url, 'use_host': False, 'use_cookie': False, 'use_post': False, 'return_data': True }
+           try:
+              data = self.cm.getURLRequestData(query_data)
+           except:
+              printDBG( 'Host listsItems query error' )
+              printDBG( 'Host listsItems query error url:'+url )
+              return valTab
+           #printDBG( 'Host listsItems data: '+data )
+           phCats = re.findall('class="categoriesbox"\sid=".*?"><a\shref="(.*?)".*?title=".*?"><img\ssrc="(.*?)"><h2>(.*?)</h2>', data, re.S)
+           if phCats:
+              for (phUrl, phImage, phTitle) in phCats:
+                  printDBG( 'Host listsItems phUrl: '  +phUrl )
+                  printDBG( 'Host listsItems phImage: '+phImage )
+                  phTitle = phTitle.replace(' movies', '')
+                  printDBG( 'Host listsItems phTitle: '+phTitle )
+                  valTab.append(CDisplayListItem(phTitle,phTitle,CDisplayListItem.TYPE_CATEGORY, [self.MAIN_URL+phUrl],'eporner-clips', phImage, None)) 
+           valTab.insert(0,CDisplayListItem("HD",        "HD",        CDisplayListItem.TYPE_CATEGORY,["http://www.eporner.com/hd/"], 'eporner-clips', '',None))
+           valTab.insert(0,CDisplayListItem("Top Rated", "Top Rated", CDisplayListItem.TYPE_CATEGORY,["http://www.eporner.com/top_rated/"], 'eporner-clips', '',None))
+           valTab.insert(0,CDisplayListItem("Popular",   "Popular",   CDisplayListItem.TYPE_CATEGORY,["http://www.eporner.com/weekly_top/"], 'eporner-clips', '',None))
+           valTab.insert(0,CDisplayListItem("On Air",    "On Air",    CDisplayListItem.TYPE_CATEGORY,["http://www.eporner.com/currently/"], 'eporner-clips', '',None))
+           valTab.insert(0,CDisplayListItem("New",       "New",       CDisplayListItem.TYPE_CATEGORY,["http://www.eporner.com/"], 'eporner-clips', '',None))
+           printDBG( 'Host listsItems end' )
+           return valTab
+        if 'eporner-clips' == name:
+           printDBG( 'Host listsItems begin name='+name )
+           query_data = { 'url': url, 'use_host': False, 'use_cookie': False, 'use_post': False, 'return_data': True }
+           try:
+              data = self.cm.getURLRequestData(query_data)
+           except:
+              printDBG( 'Host listsItems query error' )
+              printDBG( 'Host listsItems query error url: '+url )
+              return valTab
+           #printDBG( 'Host listsItems data: '+data )
+           if url == "http://www.eporner.com/top_rated/":
+              phMovies = re.findall('<div\sclass="mbtit"><a\shref="(.*?)"\stitle="(.*?)".*?src="(.*?)".*?<span>.*?<\/span>\s(.*?)<\/div>', data, re.S)
+              if phMovies:
+                 for (phUrl, phTitle, phImage, phRuntime) in phMovies:
+                  printDBG( 'Host listsItems phUrl: '  +phUrl )
+                  printDBG( 'Host listsItems phTitle: '+phTitle )
+                  printDBG( 'Host listsItems phImage: '+phImage )
+                  printDBG( 'Host listsItems phRuntime: '+phRuntime )
+                  valTab.append(CDisplayListItem(phTitle,'['+phRuntime+'] '+phTitle,CDisplayListItem.TYPE_VIDEO, [CUrlItem('', self.MAIN_URL+phUrl, 1)], 0, phImage, None)) 
+           else:
+              phMovies = re.findall('<div\sclass="mbtit"><a\shref="(.*?)"\stitle="(.*?)".*?src="(.*?)".*?<span>.*?<\/span>\s(.*?)<\/div>.*?eye.png.*?<\/span>\s(.*?)<\/div>', data, re.S)
+              if phMovies:
+                 for (phUrl, phTitle, phImage, phRuntime, phViews) in phMovies:
+                  printDBG( 'Host listsItems phUrl: '  +phUrl )
+                  printDBG( 'Host listsItems phTitle: '+phTitle )
+                  printDBG( 'Host listsItems phImage: '+phImage )
+                  printDBG( 'Host listsItems phRuntime: '+phRuntime )
+                  printDBG( 'Host listsItems phViews: '+phViews )
+                  valTab.append(CDisplayListItem(phTitle,'['+phRuntime+'] [ Views: '+phViews+'] '+phTitle,CDisplayListItem.TYPE_VIDEO, [CUrlItem('', self.MAIN_URL+phUrl, 1)], 0, phImage, None)) 
+           match = re.findall('<li class="page_next"><a href="(.*?)"', data, re.S)
+           if match:
+              valTab.append(CDisplayListItem('Next', 'Next Page', CDisplayListItem.TYPE_CATEGORY, [self.MAIN_URL+match[0].replace('&amp;','&')], name, '', None))                
            printDBG( 'Host listsItems end' )
            return valTab
 
@@ -618,6 +681,29 @@ class Host:
         except:
            printDBG( 'Host getResolvedURL query error' )
            return videoUrl
+
+        if self.MAIN_URL == 'http://www.eporner.com':
+           videoPage = re.findall(r"player4\\([^\\]*)\\", data, re.S)
+           if videoPage:
+              for phurl in videoPage:
+                  printDBG( 'Host getResolvedURL phurl: '+phurl )
+                  xml = 'http://www.eporner.com/config5%s' % (phurl)
+           printDBG( 'Host getResolvedURL xml: '+xml )
+           try:
+              data = self.cm.getURLRequestData({'url': xml, 'use_host': False, 'use_cookie': False, 'use_post': False, 'return_data': True})
+           except:
+              printDBG( 'Host getResolvedURL query error xml' )
+              return videoUrl
+           videoPage = re.findall('<hd.file>(.*?)</hd.file>', data, re.S)
+           if videoPage:
+              for phurl in videoPage:
+                  return phurl
+           else:
+              videoPage = re.findall('<file>(.*?)</file>', data, re.S)
+              if videoPage:
+                 for phurl in videoPage:
+                     return phurl
+           return ''
 
         if self.MAIN_URL == 'http://www.pornhub.com':
            match = re.compile('"video_url":"([^"]+)"').findall(data)
